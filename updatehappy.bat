5d6d1960a83370cf071bf5ecad5d3e1151c093b7
MyLegGuy <supermoronslayer@gmail.com>
2017-11-29 16:46:52 -0500

    Fix updatehappy.bat

--- updatehappy.bat
+++ updatehappy.bat
@@ -1,5 +1,5 @@
 @echo off
 echo this will copy the happy.lua from the build folder to the vpk contents folder.
 pause
-copy C:\Users\knoob\Desktop\Nathan\Programing\C\Higurashi\build\happy.lua C:\Users\knoob\Desktop\Nathan\Programing\C\Higurashi\VpkContents\a\happy.lua
+copy .\build\happy.lua .\VpkContents\assets\happy.lua
 pause
