ec51bfdab3252aaba918e19573e398920024d226
MyLegGuy <supermoronslayer@gmail.com>
2020-01-28 18:33:48 -0500

    delete customgetline
    we can use the one i put in libgoodbrew

--- stolenCode/customgetline.h
+++
@@ -1,7 +0,0 @@
-#ifndef GETLINECUSTOMINCLUDED
-#define GETLINECUSTOMINCLUDED
-#include <goodbrew/config.h>
-#include <goodbrew/base.h>
-ssize_t custom_getline(char **buf, size_t *bufsiz, crossFile fp);
-ssize_t custom_getdelim(char **buf, size_t *bufsiz, int delimiter, crossFile fp);
-#endif
